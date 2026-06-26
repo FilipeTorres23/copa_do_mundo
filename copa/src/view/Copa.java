c5a696415c1cbf023ce1ccc4701681b1e61eee1f
Davi Assunção dos Santos <82240058+davi011205@users.noreply.github.com>
2026-06-26 16:36:21 -0300

    estrutura basica
    documentacao dos autores

---
+++ copa/src/view/Copa.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package view;
+
+/**
+ *
+ * @author ANDRÉ LOPES ALVES PEREIRA MACHADO
+ * @author DAVI ASSUNÇÃO DOS SANTOS
+ * @author DIEGO PEREIRA DUARTE
+ * @author FELIPE AUGUSTO ENES DELGADO
+ * @author FILIPE HENRIQUE GONÇALVES TORRES
+ */
+public class Copa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
