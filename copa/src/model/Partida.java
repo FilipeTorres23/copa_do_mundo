b5a6ee69461dfa4182190f5ac3d4149f8c3e423f
FilipeTorres23 <torresfilipe961@gmail.com>
2026-07-06 14:47:04 -0300

    Update Partida.java

--- copa/src/model/Partida.java
+++ copa/src/model/Partida.java
@@ -1,13 +1,37 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
-/**
- *
- * @author Samsung
- */
+import service.SorteioService;
+import java.util.Random;
+
 public class Partida {
+    private Selecao usuario;
+    private Selecao adversario;
+    private SorteioService sorteador;
+
+    public Partida(Selecao usuario, Selecao adversario) {
+        this.usuario = usuario;
+        this.adversario = sorteador.sortearSelecao();
+    }
+
+    public Selecao getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Selecao usuario) {
+        this.usuario = usuario;
+    }
+
+    public Selecao getAdversario() {
+        return adversario;
+    }
+
+    public void setAdversario(Selecao adversario) {
+        this.adversario = adversario;
+    }
     
+    public ResultadoPartida jogar () {
+        Random sortearVitoria = new Random();
+        
+        
+    }
 }
